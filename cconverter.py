17ad4197fdd97379370e6dcbf26e2a6a1e5f8378
ankit9015 <67504902+ankit9015@users.noreply.github.com>
2021-05-19 11:10:03 +0530

    Delete cconverter.py

--- cconverter.py
+++
@@ -1,9 +0,0 @@
-# write your code here!
-import rates
-
-conicoin = float(input())
-
-for key, value in rates.rates.items():
-    amount = round(rates.convert(value, conicoin), 2)
-    print(f"I will get {amount} {key.upper()} from the sale of {conicoin} conicoins.")
-
